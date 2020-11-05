3aabe7c6241c95646b84cbca409569dc749861c1
politan <politan3@gmail.com>
2020-11-05 16:46:35 +0100

    Add client side error handling

--- API/Errors/ApiException.cs
+++ API/Errors/ApiException.cs
@@ -1,4 +1,16 @@
-﻿$HEADER$namespace $NAMESPACE$
+﻿namespace API.Errors
 {
-  public class $CLASS$ {$END$}
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message = null, string details = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
 }
